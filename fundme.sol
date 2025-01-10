1b2c80733607488f9d73d2b842257f74c70e5f12
Olu-wafemi <femio82@gmail.com>
2025-01-10 17:09:15 +0100

    fix issues with fund function

---
+++ fundme.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+
+pragma solidity ^0.8.18;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+contract FundMe{
+ function fund() public payable  { 
+    // require(msg.value > 1e18, "didn't send enough eth");
+
+    
+ }
+ function getPrice() public view returns(uint256){
+
+    AggregatorV3Interface pricefeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+    (, int256 price ,,,) = pricefeed.latestRoundData(); 
+
+    return uint256(price)* 1e10;
+
+ }
+}
